bcf5b0c969cdb2b83c2be196480b2f6335319247
Lucas Lopatka <lucas@bluecollardev.com>
2021-02-07 16:00:36 +0700

    Add base Model class.

--- libs/models/media/audio.ts
+++ libs/models/media/audio.ts
@@ -1,5 +1,8 @@
 import { Media } from '../abstract/media';
+import { Model } from '../abstract/model';
 
 export interface Audio extends Media {
   lengthMs?: number;
 }
+
+export class AudioModel extends Model<Audio> {}
