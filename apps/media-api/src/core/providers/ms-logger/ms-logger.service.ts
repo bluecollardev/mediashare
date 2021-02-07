8a5b52ebbf7999cef9e66488a927abc755718584
Sean Hamilton <sean.hamilton@neofinancial.com>
2021-02-07 16:05:42 -0700

    add custom logger provider

--- apps/media-api/src/core/providers/ms-logger/ms-logger.service.ts
+++ apps/media-api/src/core/providers/ms-logger/ms-logger.service.ts
@@ -1,4 +1,32 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, LoggerService } from '@nestjs/common';
 
 @Injectable()
-export class MsLoggerService {}
+export class MsLoggerService implements LoggerService {
+  private writeMessage(
+    message: any,
+    args: { context?: string; trace?: string }
+  ) {
+    const { trace = '', context = '' } = args;
+    console.log(message);
+    console.log(context);
+    console.log(trace);
+  }
+
+  constructor(private isProduction: string) {}
+
+  log(message: any, context?: string) {
+    this.writeMessage(message, { context });
+  }
+  error(message: any, trace?: string, context?: string) {
+    this.writeMessage(message, { trace, context });
+  }
+  warn(message: any, context?: string) {
+    this.writeMessage(message, { context });
+  }
+  debug?(message: any, context?: string) {
+    if (!this.isProduction) this.writeMessage(message, { context });
+  }
+  verbose?(message: any, context?: string) {
+    if (!this.isProduction) this.writeMessage(message, { context });
+  }
+}
