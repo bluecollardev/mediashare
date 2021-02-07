a29f4fe1f575a21e7a329bee4fc1474d1fbdcae2
Sean Hamilton <sean.hamilton@neofinancial.com>
2021-02-07 15:51:21 -0700

    moved to library

--- libs/core/src/index.ts
+++ libs/core/src/index.ts
@@ -1,1 +1,15 @@
 export * from './lib/core';
+
+export * from './lib/models/account.model';
+export * from './lib/models/contact.model';
+export * from './lib/models/feed.model';
+export * from './lib/models/playlist.model';
+export * from './lib/models/playlistItem.model';
+export * from './lib/models/profile.model';
+export * from './lib/models/stats.model';
+export * from './lib/models/tag.model';
+export * from './lib/models/user.model';
+
+export * from './lib/models/media/audio.model';
+export * from './lib/models/media/document.model';
+export * from './lib/models/media/video.model';
