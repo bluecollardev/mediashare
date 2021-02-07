a5bbb6e84bb435ec7903afab8731130ea7d76a54
Sean Hamilton <sean.hamilton@neofinancial.com>
2021-02-07 01:32:29 -0700

    installed something I forget what

--- apps/media-api/src/core/database/database.service.ts
+++ apps/media-api/src/core/database/database.service.ts
@@ -20,45 +20,30 @@ export class DatabaseService extends mongo.MongoClient {
     this.prefix = 'database';
   }
 
-  async insertRecord<T>(opts: {
+  insertRecord<T>(opts: {
     collection: DatabaseCollectionType;
     record: DocumentType<T>;
-  }): Promise<T> {
+  }) {
     const { collection, record } = opts;
-    try {
-      let res = await this.db(this.database)
-        .collection<T>(collection)
-        .insertOne(record);
-      if (res.insertedId) {
-        const data = res.ops[0] as any;
-        return data as T;
-      } else {
-        throw new Error(this.prefix + 'failed to insert record');
-      }
-    } catch (err) {
-      throw new Error(this.prefix + err.message);
-    }
+    return this.db(this.database).collection<T>(collection).insertOne(record);
   }
 
-  async updateRecord<T>(opts: {
+  updateRecord<T>(opts: {
     collection: DatabaseCollectionType;
     query: { [key: string]: string | Date | boolean };
     id: string;
   }): Promise<mongo.UpdateWriteOpResult> {
     const { collection, query, id } = opts;
-    const _id = new mongo.ObjectId(id);
-    try {
-      let res = await this.db(this.database)
+
+    return (
+      this.db(this.database)
         .collection<T>(collection)
         // @ts-ignore
-        .updateOne({ _id }, { $set: { ...query } });
-      return res;
-    } catch (err) {
-      throw err;
-    }
+        .updateOne({ _id }, { $set: { ...query } })
+    );
   }
 
-  async getRecords(
+  getRecords(
     collection: DatabaseCollectionType,
     opts: { count: number; batch: number; query: {} } = {
       count: 25,
@@ -67,37 +52,23 @@ export class DatabaseService extends mongo.MongoClient {
     }
   ) {
     const { count, batch, query } = opts;
-    try {
-      let res = await this.db(this.database).collection(collection).find({});
-      return await res.toArray();
-    } catch (err) {
-      throw new Error(
-        this.prefix + 'database fetch failed with ' + err.message
-      );
-    }
+    return this.db(this.database).collection(collection).find({}).toArray();
   }
 
-  async getRecord(opts: { collection: DatabaseCollectionType; id: string }) {
+  getRecord(opts: { collection: DatabaseCollectionType; id: string }) {
     const { collection, id } = opts;
     if (!id) throw new Error(this.prefix + 'no id entered');
     const _id = new mongo.ObjectId(id);
-    try {
-      let res = await this.db(this.database)
-        .collection(collection)
-        .findOne({ _id });
-      console.log(res);
-      return res;
-    } catch (err) {}
+    return this.db(this.database).collection(collection).findOne({ _id });
   }
 
-  async getRecordByQuery<T>(opts: {
+  getRecordByQuery<T>(opts: {
     collection: DatabaseCollectionType;
     query: { [key: string]: string };
   }) {
     const { collection, query } = opts;
-    let res = await this.db(this.database)
+    return this.db(this.database)
       .collection(collection)
       .findOne({ ...query });
-    return res;
   }
 }
