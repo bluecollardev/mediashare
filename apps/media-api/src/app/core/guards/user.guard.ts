6cc25864929f3c8eaf5416372e1d08f83ba96d22
Sean Hamilton <sean.hamilton@neofinancial.com>
2021-03-02 05:09:39 -0700

    JWT working now

--- apps/media-api/src/app/core/guards/user.guard.ts
+++ apps/media-api/src/app/core/guards/user.guard.ts
@@ -1,19 +1,22 @@
-import { CanActivate, Inject, ExecutionContext } from '@nestjs/common';
+import { CanActivate, Inject, ExecutionContext, Logger } from '@nestjs/common';
 import { ClientProxy } from '@nestjs/microservices';
+import { PinoLogger } from 'nestjs-pino';
 import { timeout } from 'rxjs/operators';
 
 export class UserGuard implements CanActivate {
   constructor(
     @Inject('AUTH_CLIENT')
-    private readonly client: ClientProxy
+    private readonly client: ClientProxy,
+    private logger: PinoLogger
   ) {}
 
   async canActivate(context: ExecutionContext): Promise<boolean> {
     const req = context.switchToHttp().getRequest();
-
+    Logger.warn(req.headers);
+    const { authorization } = req.headers;
     try {
       const res = await this.client
-        .send({ role: 'auth', cmd: 'check' }, { jwt: req.headers['authorization']?.split(' ')[1] })
+        .send({ role: 'auth', cmd: 'check' }, { jwt: authorization })
         .pipe(timeout(5000))
         .toPromise<boolean>();
 
