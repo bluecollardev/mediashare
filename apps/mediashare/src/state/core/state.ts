b9fbf563b79a2056b4bcfc7c0a3d73fac4d0d4fa
Lucas Lopatka <lucas@bluecollardev.com>
2021-03-10 01:08:06 +0700

    Check in changes.

---
+++ apps/mediashare/src/state/core/state.ts
@@ -0,0 +1,13 @@
+export interface IListItem {
+    id: string;
+    description: string;
+    order: number;
+}
+
+export interface IListItemsState {
+    readonly items: IListItem[]
+}
+
+export const initialListItemsState: IListItemsState = {
+    items: []
+};
