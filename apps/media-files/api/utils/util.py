3b720c4fd79e46c4211b57c6ff993ccdc2d70875
Lucas Lopatka <lucas@bluecollardev.com>
2021-03-10 00:34:46 +0700

    Add file manager / file upload api (media-files) project.

---
+++ apps/media-files/api/utils/util.py
@@ -0,0 +1,24 @@
+"""CORS pre-flight decorator.
+
+A simple decorator to add the options method to a Request Class.
+"""
+
+
+def cors_preflight(methods: str = 'GET'):
+    """Render an option method on the class."""
+
+    def wrapper(f):
+        def options(self, *args, **kwargs):  # pylint: disable=unused-argument
+            return {'Allow': 'GET'}, 200, \
+                   {'Access-Control-Allow-Origin': '*',
+                    'Access-Control-Allow-Methods': methods,
+                    'Access-Control-Allow-Headers': 'Authorization, Content-Type'}
+
+        setattr(f, 'options', options)
+        return f
+
+    return wrapper
+
+
+def validate_file():
+    return None
