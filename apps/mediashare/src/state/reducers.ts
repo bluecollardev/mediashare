62b4e887d001d6ed150d83cc6787b676963b5344
Lucas Lopatka <lucas@bluecollardev.com>
2021-03-11 11:17:28 +0700

    Combine reducers.

--- apps/mediashare/src/state/reducers.ts
+++ apps/mediashare/src/state/reducers.ts
@@ -5,10 +5,19 @@ import { combineReducers } from 'redux';
 // const rootReducer = ReducerFactory(rootReducerDict);
 
 import { userReducer, usersReducer } from './modules/user';
+import { playlistReducer, playlistsReducer, playlistItemsReducer } from './modules/playlists';
+import { mediaItemReducer, mediaItemsReducer } from './modules/media-items';
+import { shareItemsReducer } from './modules/share-items';
 
 const reducers = combineReducers({
   userReducer,
-  usersReducer
+  usersReducer,
+  mediaItemReducer,
+  mediaItemsReducer,
+  playlistReducer,
+  playlistsReducer,
+  playlistItemsReducer,
+  shareItemsReducer
 });
 
 export { reducers };
