a18c6afe325b95bdb7fee8708dd668739eb8595c
Lucas Lopatka <lucas@bluecollardev.com>
2021-03-19 10:01:15 +0700

    Tweak factory.

--- apps/mediashare/src/state/core/factory.ts
+++ apps/mediashare/src/state/core/factory.ts
@@ -6,4 +6,4 @@ export const makeEnum = <T extends string>(actions: readonly T[]): Record<SnakeC
   actions.reduce((prev, curr) => ({ ...prev, [snakeCaseToCamelCase(curr)]: curr }), Object.create({}));
 
 export const makeActions = <T extends string>(actions: readonly T[]): Record<SnakeCaseToCamelCase<T>, PayloadActionCreator> =>
-  actions.reduce((prev, curr) => ({ ...prev, [snakeCaseToCamelCase(curr)]: createAction(curr) }), Object.create({}));
+  actions.reduce((prev, curr) => ({ ...prev, [snakeCaseToCamelCase(curr)]: createAction<T>(curr) }), Object.create({}));
