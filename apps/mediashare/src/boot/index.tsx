4ef8b51e7de4602798dafa37a5369f25c8a492be
Lucas Lopatka <lucas@bluecollardev.com>
2021-09-21 07:17:15 +0700

    Updates.

--- apps/mediashare/src/boot/index.tsx
+++ apps/mediashare/src/boot/index.tsx
@@ -1,6 +1,5 @@
 import * as React from 'react';
 import { Provider } from 'react-redux';
-import { Root } from 'native-base';
 
 import { store } from './configureStore';
 import App from '../../src/App';
@@ -11,29 +10,15 @@ export interface SetupState {
   isLoading: boolean;
 }
 
-const onCompletion = () => {};
-// export const store = configureStore(onCompletion);
-
 export default class Setup extends React.Component<SetupProps, SetupState> {
   constructor(props) {
     super(props);
   }
 
   render() {
-    /*
-    <StyleProvider style={getTheme(variables)}>
-      <Provider store={this.state.store}>
-        <Root>
-          <App />
-        </Root>
-      </Provider>
-    </StyleProvider>
-    */
     return (
       <Provider store={store}>
-        <Root>
-          <App />
-        </Root>
+        <App />
       </Provider>
     );
   }
