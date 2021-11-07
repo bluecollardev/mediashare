1d635b9dc3ffbcd71c1cc1f343b9a3b633eb506b
Lucas Lopatka <lucas@bluecollardev.com>
2021-11-08 01:21:04 +0700

    This should resolve white screen on iOS. Polyfill crypto.getRandomValues using react-native-get-random-values.

--- apps/mediashare/src/boot/index.tsx
+++ apps/mediashare/src/boot/index.tsx
@@ -1,3 +1,7 @@
+// This resolves an error which occurs in environments where the standard crypto.getRandomValues() API is not supported.
+// This issue can be resolved by adding the react-native-get-random-values polyfill. We also do this in utils, but
+// adding this here ensures that the polyfill will get loaded before any crypto.getRandomValues() calls:
+import 'react-native-get-random-values';
 import * as React from 'react';
 import { Provider } from 'react-redux';
 
