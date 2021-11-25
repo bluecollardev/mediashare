cac5aafa93e87550905df45a07c0dbccd4d25b77
Lucas Lopatka <lucas@bluecollardev.com>
2021-11-25 12:17:45 +0700

    Fix a couple issues across the stack.

--- apps/mediashare/src/main.tsx
+++ apps/mediashare/src/main.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Provider } from 'react-redux';
-import App from './App';
+import App from './Application';
 import { store } from './store';
 
 export default function init() {
