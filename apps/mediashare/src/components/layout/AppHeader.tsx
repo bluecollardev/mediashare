9839a67697514207b139d1a24f0f238c130fd44d
Lucas Lopatka <lucas@bluecollardev.com>
2022-07-14 15:08:33 +0700

    Wire up clear search filter button in AppHeader.

--- apps/mediashare/src/components/layout/AppHeader.tsx
+++ apps/mediashare/src/components/layout/AppHeader.tsx
@@ -71,114 +71,17 @@ const AppHeaderComponent = ({
   const goToAccount = useGoToAccount();
 
   const title = options?.headerTitle !== undefined ? options?.headerTitle : options?.title !== undefined ? options?.title : '';
-  const placeholder = `Filter ${title}`;
 
   const searchIsFiltering = globalState?.search?.filters?.text !== '' || globalState?.search?.filters?.tags?.length > 0;
   const [searchIsActive, setSearchIsActive] = useState(false);
   const [searchText, setSearchText] = useState('');
   const [searchTags, setSearchTags] = useState([]);
 
-  const mappedTags = useMemo(() => {
-    const availableTags = Array.isArray(globalState?.tags) ? globalState.tags : [];
-    if (searchTarget === 'playlists') return availableTags.filter((tag) => tag.isPlaylistTag);
-    if (searchTarget === 'media') return availableTags.filter((tag) => tag.isMediaTag);
-    return availableTags;
-  }, []);
-
   const [displayMode, setDisplayMode] = useState(globalState?.displayMode);
 
   return (
     <Appbar.Header style={{ backgroundColor: theme.colors.background }}>
-      {searchable && searchIsActive && (
-        <Portal>
-          <SafeAreaView style={themeStyles.pageContainer}>
-            <View>
-              <Modal
-                transparent
-                visible={searchIsActive}
-                onRequestClose={() => {
-                  closeSearchConsole();
-                }}
-              >
-                <ModalContentWrapper>
-                  <BackdropView>
-                    <View
-                      style={[
-                        {
-                          overflow: 'hidden',
-                          marginHorizontal: 18,
-                          marginVertical: 26,
-                          borderRadius: 6,
-                          alignSelf: 'stretch',
-                          backgroundColor: 'transparent',
-                        },
-                      ]}
-                    >
-                      <Card>
-                        <Card.Title title={placeholder} />
-                        <Card.Content style={{ height: '89%', paddingBottom: 0 }}>
-                          <Searchbar
-                            style={{ width: '100%', marginTop: 15 }}
-                            inputStyle={{ fontSize: 15 }}
-                            placeholder="Keywords"
-                            value={searchText}
-                            onChangeText={(text) => updateSearchText(text)}
-                            onIconPress={() => closeSearchConsole()}
-                            icon=""
-                            // icon="arrow-back-ios"
-                            clearIcon="clear"
-                            autoCapitalize="none"
-                          />
-                          {/* <Appbar.Action icon="close" onPress={() => closeSearchConsole()} /> */}
-                          <SectionedMultiSelect
-                            headerComponent={() => {
-                              return (
-                                <Card>
-                                  <Card.Title title="Choose Tags" />
-                                </Card>
-                              )
-                            }}
-                            colors={components.multiSelect.colors}
-                            styles={components.multiSelect.styles}
-                            items={mappedTags}
-                            IconRenderer={MultiSelectIcon}
-                            uniqueKey="key"
-                            displayKey="value"
-                            subKey="children"
-                            searchPlaceholderText="Enter Text"
-                            selectText="Select Tags"
-                            confirmText="Save"
-                            onSelectedItemsChange={updateSearchTags}
-                            selectedItems={searchTags}
-                            hideSearch={true}
-                            showRemoveAll={false}
-                            expandDropDowns={false}
-                            readOnlyHeadings={false}
-                            showDropDowns={true}
-                            parentChipsRemoveChildren={true}
-                            showCancelButton={true}
-                            modalWithTouchable={false}
-                            modalWithSafeAreaView={false}
-                            modalAnimationType="slide"
-                          />
-                        </Card.Content>
-                      </Card>
-                      <ActionButtons
-                        containerStyles={{ marginHorizontal: 0 }}
-                        onActionClicked={() => submitSearch()}
-                        onCancelClicked={() => closeSearchConsole()}
-                      />
-                    </View>
-                  </BackdropView>
-                </ModalContentWrapper>
-              </Modal>
-            </View>
-          </SafeAreaView>
-        </Portal>
-      )}
-
       {back && <Appbar.BackAction color="#ffffff" onPress={navigation.goBack} />}
-
       <Appbar.Content
         title={title}
         titleStyle={{
@@ -188,7 +91,7 @@ const AppHeaderComponent = ({
       />
       {showDisplayControls && renderDisplayControls()}
       {searchable && showSearchSettings && <Appbar.Action icon="settings" onPress={() => undefined} />}
-      {searchable && !showSearchSettings && <Appbar.Action icon="filter-list" color={searchIsFiltering ? theme.colors.success : '#ffffff'} onPress={() => openSearchConsole()} />}
+      {searchable && !showSearchSettings && <Appbar.Action icon="filter-list" color={searchIsFiltering ? theme.colors.success : '#ffffff'} onPress={() => closeSearchConsole()} />}
       {showNotificationsMenu && <Appbar.Action icon="notifications" onPress={() => openSearchConsole()} />}
       {showAccountMenu && <Appbar.Action icon="person" onPress={() => goToAccount()} />}
     </Appbar.Header>
@@ -210,22 +113,7 @@ const AppHeaderComponent = ({
 
   function closeSearchConsole() {
     setSearchIsActive(false);
-  }
-
-  function updateSearchText(value) {
-    // Set the in-component state value
-    setSearchText(value);
-  }
-
-  function updateSearchTags(values) {
-    // Set the in-component state value
-    setSearchTags(values);
-  }
-
-  function submitSearch() {
-    // Update global search filters
-    setSearchFilters({ text: searchText, tags: [...searchTags] });
-    closeSearchConsole(); // Close the search
+    setSearchFilters({ text: '', tags: [] });
   }
 
   function renderDisplayControls() {
