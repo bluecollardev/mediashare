9e52795b0e21b11fbfbe59b36ec657bea79c0bfd
rachapol lookmuang <rachapol@kunana.ltd>
2022-07-27 21:37:20 +0700

    Revert "api user-connection"
    This reverts commit b02e7b9cd999821d735561052bcd4e1012df86b2.

--- apps/media-api/src/app/modules/user-connection/dto/update-user-connection.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateUserConnectionDto } from './create-user-connection.dto';
-
-export class UpdateUserConnectionDto extends PartialType(CreateUserConnectionDto) {}
