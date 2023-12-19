9cf601ce71e71ae7c671cca53c5225fed57fec4f
Lucas Lopatka <lucas@bluecollardev.com>
2023-12-19 22:32:28 +0700

    Deprecate CreateDto decorator.

--- apps/media-svc/src/app/core/decorators/create-dto.decorator.ts
+++ apps/media-svc/src/app/core/decorators/create-dto.decorator.ts
@@ -1,6 +1,10 @@
 import { createParamDecorator, ExecutionContext } from '@nestjs/common';
 import { ObjectIdGuard } from '@mediashare/core/guards';
 
+// TODO: Can we get rid of this?
+/**
+ * @deprecated
+ */
 export const CreateDto = createParamDecorator(
   (data, context: ExecutionContext) => {
     const ctx = context.switchToHttp().getRequest();
